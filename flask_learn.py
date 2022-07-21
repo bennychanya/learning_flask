e385d914d785d3eb7bbdce57c7f0975b7011e276
bennychanya <bennychanyongan@live.com.sg>
2022-07-21 23:55:41 +0800

    added gitignore and __pychache__ into gitignore

--- flask_learn.py
+++ flask_learn.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Benny Chan',
+        'title': 'Blog Post 1',
+        'Content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'Content': 'Second post content',
+        'date_posted': 'April 20, 2018'
+    }
+]
+
 @app.route('/')
 @app.route('/home')
 def hello():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
